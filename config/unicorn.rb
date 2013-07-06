bf5e25789d96bf32a1a1f9b02bda44f6ce0462b1
James Adney <jfadney@gmail.com>
2013-07-05 19:50:33 -0700

    setup to run on one dyno

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,9 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
-preload_app true
+worker_processes 3
+timeout 30
+
+@resque_pid = nil
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  @resque_pid ||= spawn("bundle exec rake " + \
+  "resque:work QUEUE=carrierwave")
 end
